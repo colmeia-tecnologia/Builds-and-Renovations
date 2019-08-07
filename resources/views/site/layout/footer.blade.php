c0d1657596cb6d642c213afa6f6de9b228284b99
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-08-07 15:05:12 -0400

    Site - Social Medias

--- resources/views/site/layout/footer.blade.php
+++ resources/views/site/layout/footer.blade.php
@@ -3,11 +3,13 @@
         <a href='#' id='backToTop' class='btn grey darken-4 white-text'>Back to top</a>
     </div>
     <div class='center primary'>
-        @foreach (Cache::get('socialMedias') as $socialMedia)
-            <a href='{{$socialMedia->url}}' title='{{$socialMedia->name}}' class='grey-text text-darken-4'>
-                {!!$socialMedia->icon!!}
-            </a>
-        @endforeach
+        <div class='socialMedias'>
+            @foreach (Cache::get('socialMedias') as $socialMedia)
+                <a href='{{$socialMedia->url}}' title='{{$socialMedia->name}}' class='grey-text text-darken-4'>
+                    <img src='{{$socialMedia->icon}}' title='{{$socialMedia->name}}' alt='{{$socialMedia->name}}' class='img-resonsive'>
+                </a>
+            @endforeach
+        </div>
 
         <hr class='white margin-top-g margin-bottom-p'>
 
